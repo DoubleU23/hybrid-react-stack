d6520db289281d4b4a0cd955af8b7b8030153451
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2026-06-15 22:29:40 +0200

    cleanup

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,18 +1,8 @@
-import {Routes, Route } from "react-router-dom";
-import { ReactNode } from "react";
 import App from "./App";
 import NotFound from "./NotFound/NotFound";
 import Home from "./Home/Home";
 
-
-interface routeObject {
-    name?: string,
-    path?: string
-    Component: ReactNode,
-    children?: Array<routeObject>
-}
-
-const AppRoutes/* :routeObject[] */ = [
+const AppRoutes = [
   {
     Component: App,
     children: [
@@ -22,16 +12,5 @@ const AppRoutes/* :routeObject[] */ = [
 
   },
 ]
-
-/* <Route path="/" element={<Home />} /> */
-
-/* function AppRoutes(params:any) {
-    return (
-        <Routes> 
-            {routesArray.map(({name, path, element}):ReactNode=>(<Route path={path} element={element} />))}
-            <Route path="*" element={<NotFound />} /> 
-      </Routes>
-    )    
-} */
 
 export default AppRoutes
