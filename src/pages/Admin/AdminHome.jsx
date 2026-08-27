30f3e74b1c7c9ddf5ae686cf914fdba0bf9ecc46
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2026-08-28 01:39:18 +0200

    fix relative src path

--- src/pages/Admin/AdminHome.jsx
+++ src/pages/Admin/AdminHome.jsx
@@ -1,12 +1,9 @@
-import PageContainer from "src/components/mui/PageContainer"
-import Box from "@mui/material/Box"
+import Box from '@mui/material/Box'
+import PageContainer from '../components/mui/PageContainer'
 export default function AdminHome(params) {
-
-    return (
-        <PageContainer title='Dashboard' breadcrumbs={[{ title: 'Admin', path: '/admin' }, { title: 'Dashboard' }]}>
-        <Box sx={{ flex: 1, width: '100%' }}>
-                    ADMIN HOME
-        </Box>
-        </PageContainer>
-    )
+  return (
+    <PageContainer title='Dashboard' breadcrumbs={[{ title: 'Admin', path: '/admin' }, { title: 'Dashboard' }]}>
+      <Box sx={{ flex: 1, width: '100%' }}>ADMIN HOME</Box>
+    </PageContainer>
+  )
 }
