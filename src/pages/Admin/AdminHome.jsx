fdfac106a2be2e6c62857395f6f190c10ed25bfa
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2026-08-28 01:57:23 +0200

    fix import path

--- src/pages/Admin/AdminHome.jsx
+++ src/pages/Admin/AdminHome.jsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box'
-import PageContainer from '../components/mui/PageContainer'
+import PageContainer from '../../components/mui/PageContainer'
+
 export default function AdminHome(params) {
   return (
     <PageContainer title='Dashboard' breadcrumbs={[{ title: 'Admin', path: '/admin' }, { title: 'Dashboard' }]}>
