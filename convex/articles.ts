a223b71dae62539a98f65291f7b515b8ec015119
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2026-08-28 01:42:23 +0200

    remove failed import

--- convex/articles.ts
+++ convex/articles.ts
@@ -1,6 +1,5 @@
 import { v } from "convex/values"
 import { mutation, query } from "./_generated/server"
-import type { Id } from "./_generated/dataModel"
 import { articleValidator } from "./schema"
 
   export const getArticleById = query({
